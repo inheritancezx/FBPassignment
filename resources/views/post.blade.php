f123802c5b00dd590c7513ae4fcfb102ee4ac860
inheritancezx <fayzaaqilaar@gmail.com>
2024-10-03 08:32:16 +0700

    fix: category post in single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,11 @@
     <article class="py-8 max-w-screen-md">
         <h3 class="mb-1 text-2xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h3>
         <div class="text-base text-gray-500">
-            <a href="authors/{{ $post->author->username }}">{{ $post->author->name }}</a> | 14 April 2023
+            by
+            <a href="authors/{{ $post->author->username }}">{{ $post->author->name }}</a> 
+            in
+            <a href="categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            | {{ $post->created_at->format('j F Y') }}
         </div>
         <p class="my-4 font-light">{{ $post['content'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; back to posts</a>
